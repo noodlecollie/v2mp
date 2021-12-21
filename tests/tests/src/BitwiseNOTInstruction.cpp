95fa040bfbd5297ca3a3df3cab2a2bb983170c31
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2021-12-21 21:17:49 +0000

    Improved NOT tests

--- tests/tests/src/BitwiseNOTInstruction.cpp
+++ tests/tests/src/BitwiseNOTInstruction.cpp
@@ -4,6 +4,8 @@
 
 static constexpr V2MP_Word TEST_VALUE = 0xFED5;
 
+// Note that source registers are (and should be) ignored for a NOT operation.
+// This test scenario sets them in the instruction anyway, to ensure that they are ignored.
 SCENARIO("BITW: Performing a bitwise NOT on a register results in the correct value in the register", "[instructions]")
 {
 	GIVEN("A virtual machine")
@@ -16,17 +18,60 @@ SCENARIO("BITW: Performing a bitwise NOT on a register results in the correct va
 
 			vm.SetR0(TEST_VALUE);
 
-			WHEN("A bitwise NOT is performed")
+			AND_GIVEN("The source register is R1")
 			{
-				REQUIRE(vm.Execute(Asm::BITWR(REG_DEST, REG_DEST, Asm::BitwiseOp::NOT)));
+				static constexpr uint8_t REG_SRC = Asm::REG_R1;
 
-				THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+				WHEN("A bitwise NOT is performed")
 				{
-					CHECK(vm.GetR0() == static_cast<V2MP_Word>(~TEST_VALUE));
-					CHECK(vm.GetR1() == 0);
-					CHECK(vm.GetLR() == 0);
-					CHECK(vm.GetPC() == 0);
-					CHECK_FALSE(vm.CPUHasFault());
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is LR")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_LR;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is PC")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_PC;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
 				}
 			}
 		}
@@ -37,17 +82,60 @@ SCENARIO("BITW: Performing a bitwise NOT on a register results in the correct va
 
 			vm.SetR1(TEST_VALUE);
 
-			WHEN("A bitwise NOT is performed")
+			AND_GIVEN("The source register is R0")
 			{
-				REQUIRE(vm.Execute(Asm::BITWR(REG_DEST, REG_DEST, Asm::BitwiseOp::NOT)));
+				static constexpr uint8_t REG_SRC = Asm::REG_R0;
 
-				THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+				WHEN("A bitwise NOT is performed")
 				{
-					CHECK(vm.GetR0() == 0);
-					CHECK(vm.GetR1() == static_cast<V2MP_Word>(~TEST_VALUE));
-					CHECK(vm.GetLR() == 0);
-					CHECK(vm.GetPC() == 0);
-					CHECK_FALSE(vm.CPUHasFault());
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is LR")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_LR;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is PC")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_PC;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
 				}
 			}
 		}
@@ -58,17 +146,60 @@ SCENARIO("BITW: Performing a bitwise NOT on a register results in the correct va
 
 			vm.SetLR(TEST_VALUE);
 
-			WHEN("A bitwise NOT is performed")
+			AND_GIVEN("The source register is R0")
 			{
-				REQUIRE(vm.Execute(Asm::BITWR(REG_DEST, REG_DEST, Asm::BitwiseOp::NOT)));
+				static constexpr uint8_t REG_SRC = Asm::REG_R0;
 
-				THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+				WHEN("A bitwise NOT is performed")
 				{
-					CHECK(vm.GetR0() == 0);
-					CHECK(vm.GetR1() == 0);
-					CHECK(vm.GetLR() == static_cast<V2MP_Word>(~TEST_VALUE));
-					CHECK(vm.GetPC() == 0);
-					CHECK_FALSE(vm.CPUHasFault());
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is R1")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_R1;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is PC")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_PC;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK(vm.GetPC() == 0);
+						CHECK_FALSE(vm.CPUHasFault());
+					}
 				}
 			}
 		}
@@ -79,17 +210,60 @@ SCENARIO("BITW: Performing a bitwise NOT on a register results in the correct va
 
 			vm.SetPC(TEST_VALUE);
 
-			WHEN("A bitwise NOT is performed")
+			AND_GIVEN("The source register is R0")
 			{
-				REQUIRE(vm.Execute(Asm::BITWR(REG_DEST, REG_DEST, Asm::BitwiseOp::NOT)));
+				static constexpr uint8_t REG_SRC = Asm::REG_R0;
 
-				THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+				WHEN("A bitwise NOT is performed")
 				{
-					CHECK(vm.GetR0() == 0);
-					CHECK(vm.GetR1() == 0);
-					CHECK(vm.GetLR() == 0);
-					CHECK(vm.GetPC() == static_cast<V2MP_Word>(~TEST_VALUE));
-					CHECK_FALSE(vm.CPUHasFault());
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is R1")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_R1;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK_FALSE(vm.CPUHasFault());
+					}
+				}
+			}
+
+			AND_GIVEN("The source register is LR")
+			{
+				static constexpr uint8_t REG_SRC = Asm::REG_LR;
+
+				WHEN("A bitwise NOT is performed")
+				{
+					REQUIRE(vm.Execute(Asm::BITWR(REG_SRC, REG_DEST, Asm::BitwiseOp::NOT)));
+
+					THEN("The destination register's value should be bitwise inverted, and all other registers should be unchanged")
+					{
+						CHECK(vm.GetR0() == 0);
+						CHECK(vm.GetR1() == 0);
+						CHECK(vm.GetLR() == 0);
+						CHECK(vm.GetPC() == static_cast<V2MP_Word>(~TEST_VALUE));
+						CHECK_FALSE(vm.CPUHasFault());
+					}
 				}
 			}
 		}
