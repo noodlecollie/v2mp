670aae42c9852f47dc48c8311d7a72d785a7ac68
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2021-12-28 14:01:46 +0000

    Updated BITW AND tests

--- tests/tests/src/Instructions/BitwiseANDInstruction.cpp
+++ tests/tests/src/Instructions/BitwiseANDInstruction.cpp
@@ -1,7 +1,5 @@
-#define OLD_TEST
-
 #include "catch2/catch.hpp"
-#include "Helpers/MinimalVirtualMachine.h"
+#include "Helpers/TestHarnessVM.h"
 #include "Helpers/Assembly.h"
 
 static constexpr V2MP_Word TEST_VALUE = 0xFED5;
@@ -12,7 +10,7 @@ SCENARIO("BITW: Performing a bitwise AND between two registers results in the co
 {
 	GIVEN("A virtual machine")
 	{
-		MinimalVirtualMachine vm;
+		TestHarnessVM vm;
 
 		AND_GIVEN("The destination register is R0")
 		{
@@ -588,7 +586,7 @@ SCENARIO("BITW: Performing a bitwise AND using a constructed mask results in the
 {
 	GIVEN("A virtual machine")
 	{
-		MinimalVirtualMachine vm;
+		TestHarnessVM vm;
 
 		AND_GIVEN("The destination register is R0, filled with all-ones")
 		{
@@ -805,7 +803,7 @@ SCENARIO("BITW: Performing a bitwise AND between two registers with other operan
 		static constexpr V2MP_Word VAL_LR = 0x0003;
 		static constexpr V2MP_Word VAL_PC = 0x0004;
 
-		MinimalVirtualMachine vm;
+		TestHarnessVM vm;
 
 		vm.SetR0(VAL_R0);
 		vm.SetR1(VAL_R1);
@@ -821,7 +819,7 @@ SCENARIO("BITW: Performing a bitwise AND between two registers with other operan
 				THEN("A RES fault is raised, and all registers are left unchanged")
 				{
 					CHECK(vm.CPUHasFault());
-					CHECK(Asm::FaultFromWord(vm.GetCPUFault()) == V2MP_FAULT_RES);
+					CHECK(Asm::FaultFromWord(vm.GetCPUFaultWord()) == V2MP_FAULT_RES);
 					CHECK(vm.GetR0() == VAL_R0);
 					CHECK(vm.GetR1() == VAL_R1);
 					CHECK(vm.GetLR() == VAL_LR);
