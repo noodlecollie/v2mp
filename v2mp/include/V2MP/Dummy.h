8903600f5ddd42f18a845c123e4d91f467e2ecff
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2021-12-28 15:33:48 +0000

    Rearranged/removed files that are no longer needed

---
+++ v2mp/include/V2MP/Dummy.h
@@ -0,0 +1,16 @@
+#ifndef V2MP_DUMMY_H
+#define V2MP_DUMMY_H
+
+#include "V2MP/LibExport.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+API_V2MP int V2MP_Dummy(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // V2MP_DUMMY_H
---
+++ v2mp/src/Dummy.c
@@ -0,0 +1,6 @@
+#include "V2MP/Dummy.h"
+
+int V2MP_Dummy(void)
+{
+	return 0;
+}
