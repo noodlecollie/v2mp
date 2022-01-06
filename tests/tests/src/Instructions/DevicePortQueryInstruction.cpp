e48b281d52387ed2d305c22646b8558851995704
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2022-01-06 21:39:58 +0000

    Added more DPQ tests
    One still fails because the supervisor actions need to be upgraded

--- tests/tests/src/Instructions/DevicePortQueryInstruction.cpp
+++ tests/tests/src/Instructions/DevicePortQueryInstruction.cpp
@@ -59,12 +59,10 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 	{
 		static constexpr V2MP_Word PORT_NUMBER = 345;
 
-		TestHarnessVM vm;
+		TestHarnessVM vm(2 * sizeof(V2MP_Word));
 
 		V2MP_DevicePort* port = vm.CreatePort(PORT_NUMBER);
 		REQUIRE(port);
-
-		vm.SetR0(PORT_NUMBER);
 
 		AND_GIVEN("The port is not connected to a device")
 		{
@@ -72,6 +70,7 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 
 			WHEN("The port's connected state is queried")
 			{
+				vm.SetR0(PORT_NUMBER);
 				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
 
 				THEN("SR[Z] is set")
@@ -94,6 +93,7 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 
 			WHEN("The port's R+NB state is queried")
 			{
+				vm.SetR0(PORT_NUMBER);
 				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
 
 				THEN("SR[Z] is set")
@@ -120,6 +120,7 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 
 			WHEN("The port's R+NB state is queried")
 			{
+				vm.SetR0(PORT_NUMBER);
 				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
 
 				THEN("SR[Z] is set")
@@ -147,6 +148,7 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 
 			WHEN("The port's R+NB state is queried")
 			{
+				vm.SetR0(PORT_NUMBER);
 				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
 
 				THEN("SR[Z] is not set")
@@ -155,8 +157,34 @@ SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the corre
 					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
 				}
 			}
-		}
 
-		// TODO: Test for busy state
+			AND_GIVEN("A device port read is in progress")
+			{
+				vm.FillCSAndDS(1, 0, 1, 0);
+				vm.SetR0(PORT_NUMBER);
+				vm.SetLR(0);
+				vm.SetR1(2);
+
+				device->SetDataTransferSpeed(1);
+
+				REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::READ, true)));
+				REQUIRE_FALSE(vm.CPUHasFault());
+				REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_SUPERVISOR);
+				REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_READABLE);
+				REQUIRE(V2MP_DevicePort_IsMailboxBusy(port));
+
+				WHEN("The port's R+NB state is queried")
+				{
+					vm.SetR0(PORT_NUMBER);
+					REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
+
+					THEN("SR[Z] is set")
+					{
+						REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
+						REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
+					}
+				}
+			}
+		}
 	}
 }
