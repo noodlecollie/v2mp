a0c66f12fc99bb50ea6eb46562ea0197c6d72bf8
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2022-01-16 13:09:41 +0000

    Added all DPQ tests

--- tests/tests/src/Instructions/DevicePortQueryInstruction.cpp
+++ tests/tests/src/Instructions/DevicePortQueryInstruction.cpp
@@ -11,10 +11,12 @@ struct QueryAffirmitive
 	bool whenMailboxReadableAndNotBusy = false;
 	bool whenMailboxReadableAndBusy = false;
 	bool whenMailboxExhaustedAfterRead = false;
+	bool whenMailboxRelinquishedAfterRead = false;
 
 	bool whenMailboxWriteableAndNotBusy = false;
 	bool whenMailboxWriteableAndBusy = false;
 	bool whenMailboxExhaustedAfterWrite = false;
+	bool whenMailboxRelinquishedAfterWrite = false;
 };
 
 static inline bool QueryResultIsAffirmitive(const TestHarnessVM& vm)
@@ -156,6 +158,27 @@ static void TestAllStatesWithQuery(Asm::DevicePortQuery query, const QueryAffirm
 							REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
 						}
 					}
+
+					AND_GIVEN("The mailbox is relinquished by the program")
+					{
+						REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::RELINQUISH_MAILBOX, false)));
+						REQUIRE_FALSE(vm.CPUHasFault());
+						REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_UNAVAILABLE);
+
+						WHEN("The query is executed")
+						{
+							vm.SetR0(PORT_NUMBER);
+
+							REQUIRE(vm.Execute(Asm::DPQ(query)));
+							REQUIRE_FALSE(vm.CPUHasFault());
+
+							THEN("The result is correct")
+							{
+								REQUIRE(QueryResultIsAffirmitive(vm) == affirmitive.whenMailboxRelinquishedAfterRead);
+								REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
+							}
+						}
+					}
 				}
 			}
 		}
@@ -236,6 +259,27 @@ static void TestAllStatesWithQuery(Asm::DevicePortQuery query, const QueryAffirm
 							REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
 						}
 					}
+
+					AND_GIVEN("The mailbox is relinquished by the program")
+					{
+						REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::RELINQUISH_MAILBOX, false)));
+						REQUIRE_FALSE(vm.CPUHasFault());
+						REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_UNAVAILABLE);
+
+						WHEN("The query is executed")
+						{
+							vm.SetR0(PORT_NUMBER);
+
+							REQUIRE(vm.Execute(Asm::DPQ(query)));
+							REQUIRE_FALSE(vm.CPUHasFault());
+
+							THEN("The result is correct")
+							{
+								REQUIRE(QueryResultIsAffirmitive(vm) == affirmitive.whenMailboxRelinquishedAfterWrite);
+								REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
+							}
+						}
+					}
 				}
 			}
 		}
@@ -244,406 +288,42 @@ static void TestAllStatesWithQuery(Asm::DevicePortQuery query, const QueryAffirm
 
 SCENARIO("DPQ: Querying a port's connected state returns the correct answer", "[instructions]")
 {
-	GIVEN("A virtual machine and a device port")
-	{
-		static constexpr V2MP_Word PORT_NUMBER = 345;
+	QueryAffirmitive affirmitive;
 
-		TestHarnessVM vm;
+	affirmitive.WhenConnectedWithNoMailbox = true;
 
-		V2MP_DevicePort* port = vm.CreatePort(PORT_NUMBER);
-		REQUIRE(port);
+	affirmitive.whenMailboxReadableAndNotBusy = true;
+	affirmitive.whenMailboxReadableAndBusy = true;
+	affirmitive.whenMailboxExhaustedAfterRead = true;
+	affirmitive.whenMailboxRelinquishedAfterRead = true;
 
-		vm.SetR0(PORT_NUMBER);
+	affirmitive.whenMailboxWriteableAndNotBusy = true;
+	affirmitive.whenMailboxWriteableAndBusy = true;
+	affirmitive.whenMailboxExhaustedAfterWrite = true;
+	affirmitive.whenMailboxRelinquishedAfterWrite = true;
 
-		AND_GIVEN("The port is not connected to a device")
-		{
-			REQUIRE_FALSE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			WHEN("The port's connected state is queried")
-			{
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::CONNECTED)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device")
-		{
-			std::shared_ptr<BaseMockDevice> device = vm.ConnectMockDeviceToPort<BaseMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			WHEN("The port's connected state is queried")
-			{
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::CONNECTED)));
-
-				THEN("SR[Z] is not set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) == 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-	}
+	TestAllStatesWithQuery(Asm::DevicePortQuery::CONNECTED, affirmitive);
 }
 
-SCENARIO("DPQ: Querying a port's \"readable + not busy\" state returns the correct answer", "[instructions]")
+SCENARIO("DPQ: Querying a port mailbox's \"readable + not busy\" state returns the correct answer", "[instructions]")
 {
-	GIVEN("A virtual machine and a device port")
-	{
-		static constexpr V2MP_Word PORT_NUMBER = 345;
+	QueryAffirmitive affirmitive;
 
-		TestHarnessVM vm(2 * sizeof(V2MP_Word));
+	affirmitive.whenMailboxReadableAndNotBusy = true;
 
-		V2MP_DevicePort* port = vm.CreatePort(PORT_NUMBER);
-		REQUIRE(port);
-
-		AND_GIVEN("The port is not connected to a device")
-		{
-			REQUIRE_FALSE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			WHEN("The port's R+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which has no mailbox")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-			REQUIRE_FALSE(V2MP_DevicePort_HasMailbox(port));
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_UNAVAILABLE);
-
-			WHEN("The port's R+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which leaves the mailbox writeable")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			device->AllocateConnectedMailbox(64);
-
-			REQUIRE(device->RelinquishConnectedMailbox());
-			REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_PROGRAM);
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_WRITEABLE);
-
-			WHEN("The port's R+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which leaves the mailbox readable")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			device->AllocateConnectedMailbox(64);
-			device->WriteToConnectedMailbox("Hello");
-
-			REQUIRE(device->RelinquishConnectedMailbox());
-			REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_PROGRAM);
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_READABLE);
-
-			WHEN("The port's R+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is not set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) == 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-
-			AND_GIVEN("A device port read is in progress")
-			{
-				vm.FillCSAndDS(1, 0, 1, 0);
-				vm.SetR0(PORT_NUMBER);
-				vm.SetLR(0);
-				vm.SetR1(2);
-
-				device->SetDataTransferSpeed(1);
-
-				REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::READ, true)));
-				REQUIRE_FALSE(vm.CPUHasFault());
-				REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_READABLE);
-				REQUIRE(V2MP_DevicePort_IsMailboxBusy(port));
-
-				WHEN("The port's R+NB state is queried")
-				{
-					vm.SetR0(PORT_NUMBER);
-					REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-					THEN("SR[Z] is set")
-					{
-						REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-						REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-					}
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which leaves the mailbox writeable")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			device->AllocateConnectedMailbox(64);
-
-			REQUIRE(device->RelinquishConnectedMailbox());
-			REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_PROGRAM);
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_WRITEABLE);
-
-			WHEN("The port's R+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-
-			AND_GIVEN("A device port write is in progress")
-			{
-				vm.FillCSAndDS(1, 0, 1, 0);
-				vm.SetR0(PORT_NUMBER);
-				vm.SetLR(0);
-				vm.SetR1(2);
-
-				device->SetDataTransferSpeed(1);
-
-				REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::WRITE, true)));
-				REQUIRE_FALSE(vm.CPUHasFault());
-				REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_WRITEABLE);
-				REQUIRE(V2MP_DevicePort_IsMailboxBusy(port));
-
-				WHEN("The port's R+NB state is queried")
-				{
-					vm.SetR0(PORT_NUMBER);
-					REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::READABLE_NOT_BUSY)));
-
-					THEN("SR[Z] is set")
-					{
-						REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-						REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-					}
-				}
-			}
-		}
-	}
+	TestAllStatesWithQuery(Asm::DevicePortQuery::READABLE_NOT_BUSY, affirmitive);
 }
 
-SCENARIO("DPQ: Querying a port's \"writeable + not busy\" state returns the correct answer", "[instructions]")
+SCENARIO("DPQ: Querying a port mailbox's \"writeable + not busy\" state returns the correct answer", "[instructions]")
 {
-	GIVEN("A virtual machine and a device port")
-	{
-		static constexpr V2MP_Word PORT_NUMBER = 345;
+	QueryAffirmitive affirmitive;
 
-		TestHarnessVM vm(2 * sizeof(V2MP_Word));
+	affirmitive.whenMailboxWriteableAndNotBusy = true;
 
-		V2MP_DevicePort* port = vm.CreatePort(PORT_NUMBER);
-		REQUIRE(port);
-
-		AND_GIVEN("The port is not connected to a device")
-		{
-			REQUIRE_FALSE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			WHEN("The port's W+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which has no mailbox")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-			REQUIRE_FALSE(V2MP_DevicePort_HasMailbox(port));
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_UNAVAILABLE);
-
-			WHEN("The port's W+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which leaves the mailbox readable")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			device->AllocateConnectedMailbox(64);
-			device->WriteToConnectedMailbox("Hello");
-
-			REQUIRE(device->RelinquishConnectedMailbox());
-			REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_PROGRAM);
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_READABLE);
-
-			WHEN("The port's W+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-
-			AND_GIVEN("A device port read is in progress")
-			{
-				vm.FillCSAndDS(1, 0, 1, 0);
-				vm.SetR0(PORT_NUMBER);
-				vm.SetLR(0);
-				vm.SetR1(2);
-
-				device->SetDataTransferSpeed(1);
-
-				REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::READ, true)));
-				REQUIRE_FALSE(vm.CPUHasFault());
-				REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_READABLE);
-				REQUIRE(V2MP_DevicePort_IsMailboxBusy(port));
-
-				WHEN("The port's W+NB state is queried")
-				{
-					vm.SetR0(PORT_NUMBER);
-					REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-					THEN("SR[Z] is set")
-					{
-						REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-						REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-					}
-				}
-			}
-		}
-
-		AND_GIVEN("The port is connected to a device which leaves the mailbox writeable")
-		{
-			std::shared_ptr<EmitterMockDevice> device = vm.ConnectMockDeviceToPort<EmitterMockDevice>(PORT_NUMBER);
-
-			REQUIRE(device);
-			REQUIRE(V2MP_DevicePort_HasConnectedDevice(port));
-
-			device->AllocateConnectedMailbox(64);
-
-			REQUIRE(device->RelinquishConnectedMailbox());
-			REQUIRE(V2MP_DevicePort_GetMailboxController(port) == V2MP_DMBC_PROGRAM);
-			REQUIRE_FALSE(V2MP_DevicePort_IsMailboxBusy(port));
-			REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_WRITEABLE);
-
-			WHEN("The port's W+NB state is queried")
-			{
-				vm.SetR0(PORT_NUMBER);
-				REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-				THEN("SR[Z] is not set")
-				{
-					REQUIRE((vm.GetSR() & Asm::SR_Z) == 0);
-					REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-				}
-			}
-
-			AND_GIVEN("A device port write is in progress")
-			{
-				vm.FillCSAndDS(1, 0, 1, 0);
-				vm.SetR0(PORT_NUMBER);
-				vm.SetLR(0);
-				vm.SetR1(2);
-
-				device->SetDataTransferSpeed(1);
-
-				REQUIRE(vm.Execute(Asm::DPO(Asm::DevicePortOperation::WRITE, true)));
-				REQUIRE_FALSE(vm.CPUHasFault());
-				REQUIRE(V2MP_DevicePort_GetMailboxState(port) == V2MP_DPMS_WRITEABLE);
-				REQUIRE(V2MP_DevicePort_IsMailboxBusy(port));
-
-				WHEN("The port's W+NB state is queried")
-				{
-					vm.SetR0(PORT_NUMBER);
-					REQUIRE(vm.Execute(Asm::DPQ(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY)));
-
-					THEN("SR[Z] is set")
-					{
-						REQUIRE((vm.GetSR() & Asm::SR_Z) != 0);
-						REQUIRE((vm.GetSR() & ~Asm::SR_Z) == 0);
-					}
-				}
-			}
-		}
-	}
+	TestAllStatesWithQuery(Asm::DevicePortQuery::WRITEABLE_NOT_BUSY, affirmitive);
 }
 
-SCENARIO("DPQ: Querying a port's exhausted state returns the correct answer", "[instructions]")
+SCENARIO("DPQ: Querying a port mailbox's exhausted state returns the correct answer", "[instructions]")
 {
 	QueryAffirmitive affirmitive;
 
@@ -651,4 +331,28 @@ SCENARIO("DPQ: Querying a port's exhausted state returns the correct answer", "[
 	affirmitive.whenMailboxExhaustedAfterWrite = true;
 
 	TestAllStatesWithQuery(Asm::DevicePortQuery::EXHAUSTED, affirmitive);
+}
+
+SCENARIO("DPQ: Querying a port mailbox's busy state returns the correct answer", "[instructions]")
+{
+	QueryAffirmitive affirmitive;
+
+	affirmitive.whenMailboxReadableAndBusy = true;
+	affirmitive.whenMailboxWriteableAndBusy = true;
+
+	TestAllStatesWithQuery(Asm::DevicePortQuery::BUSY, affirmitive);
+}
+
+SCENARIO("DPQ: Querying whether a port's mailbox is controlled by the program returns the correct answer", "[instructions]")
+{
+	QueryAffirmitive affirmitive;
+
+	affirmitive.whenMailboxReadableAndNotBusy = true;
+	affirmitive.whenMailboxReadableAndBusy = true;
+	affirmitive.whenMailboxExhaustedAfterRead = true;
+	affirmitive.whenMailboxWriteableAndNotBusy = true;
+	affirmitive.whenMailboxWriteableAndBusy = true;
+	affirmitive.whenMailboxExhaustedAfterWrite = true;
+
+	TestAllStatesWithQuery(Asm::DevicePortQuery::CONTROLLED_BY_PROGRAM, affirmitive);
 }
