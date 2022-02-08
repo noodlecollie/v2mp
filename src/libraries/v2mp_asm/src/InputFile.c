5a6c0455f981b9da75f38c750264fcdc3c402859
Vesper <3692034+noodlecollie@users.noreply.github.com>
2022-02-08 11:35:28 +0000

    Added basic InputFile functions

--- src/libraries/v2mp_asm/src/InputFile.c
+++ src/libraries/v2mp_asm/src/InputFile.c
@@ -1,21 +1,45 @@
 #include "V2MPAsm/InputFile.h"
+#include "BaseUtil/Heap.h"
+
+struct V2MPAsm_InputFile
+{
+	const V2MPAsm_Byte* data;
+	size_t length;
+};
 
 V2MPAsm_InputFile* V2MPAsm_InputFile_AllocateAndInit(void)
 {
-	// TODO
-	return NULL;
+	V2MPAsm_InputFile* inputFile;
+
+	inputFile = BASEUTIL_CALLOC_STRUCT(V2MPAsm_InputFile);
+
+	return inputFile;
 }
 
 void V2MPAsm_InputFile_DeinitAndFree(V2MPAsm_InputFile* inputFile)
 {
-	// TODO
-	(void)inputFile;
+	if ( !inputFile )
+	{
+		return;
+	}
+
+	BASEUTIL_FREE(inputFile);
 }
 
 void V2MPAsm_InputFile_SetInput(V2MPAsm_InputFile* inputFile, const V2MPAsm_Byte* data, size_t length)
 {
-	// TODO
-	(void)inputFile;
-	(void)data;
-	(void)length;
+	if ( !inputFile )
+	{
+		return;
+	}
+
+	if ( !data || length < 1 )
+	{
+		inputFile->data = NULL;
+		inputFile->length = 0;
+		return;
+	}
+
+	inputFile->data = data;
+	inputFile->length = length;
 }
