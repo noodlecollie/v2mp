2ba7a08f76e0d04b463fbf372422ea76029eec9a
Vesper <3692034+noodlecollie@users.noreply.github.com>
2022-02-18 08:32:51 +0000

    Added codeword descriptor list

---
+++ src/libraries/v2mp_asm/src/CodewordDescriptors/CWDList.h
@@ -0,0 +1,20 @@
+#ifndef V2MPASM_CWDLIST_H
+#define V2MPASM_CWDLIST_H
+
+#include <stddef.h>
+#include "CodewordDescriptors/CWD_Base.h"
+
+typedef struct V2MPAsm_CWDList
+{
+	V2MPAsm_CWDBase** head;
+	size_t size;
+	size_t capacity;
+} V2MPAsm_CWDList;
+
+V2MPAsm_CWDList* V2MPAsm_CWDList_AllocateAndInit(void);
+void V2MPAsm_CWDList_DeinitAndFree(V2MPAsm_CWDList* list);
+
+size_t V2MPAsm_CWDList_GetSize(const V2MPAsm_CWDList* list);
+V2MPAsm_CWDBase* V2MPAsm_CWDList_AppendNew(V2MPAsm_CWDList* list, V2MPAsm_CWD_Type type);
+
+#endif // V2MPASM_CWDLIST_H
