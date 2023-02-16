0aeef1695560b372db62e7fd3093f51cfacbf3c9
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2023-02-16 19:30:03 +0000

    Added skeleton linker DLL

--- src/executables/v2mplink/src/Main.cpp
+++ src/executables/v2mplink/src/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "LibV2MPLink/Dummy.h"
 
 int main(int /* argc */, char** /* argv */)
 {
+	std::cout << "Return value: " << V2MPLink_Dummy() << std::endl;
 	return 0;
 }
---
+++ src/libraries/libv2mplink/src/Dummy.cpp
@@ -0,0 +1,6 @@
+#include "LibV2MPLink/Dummy.h"
+
+int V2MPLink_Dummy(void)
+{
+	return 0;
+}
