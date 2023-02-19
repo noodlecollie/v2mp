b5bbc48fdcf2674bf8e4167650ff526d26ad69fc
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2023-02-19 19:52:36 +0000

    Improved version functions

--- src/libraries/libv2mpasm/include/LibV2MPAsm/Version.h
+++ src/libraries/libv2mpasm/include/LibV2MPAsm/Version.h
@@ -12,6 +12,7 @@ API_LIBV2MPASM int32_t V2MPAsm_Version_GetMajor(void);
 API_LIBV2MPASM int32_t V2MPAsm_Version_GetMinor(void);
 API_LIBV2MPASM int32_t V2MPAsm_Version_GetPatch(void);
 API_LIBV2MPASM const char* V2MPAsm_Version_GetVersionString(void);
+API_LIBV2MPASM const char* V2MPAsm_Version_GetBuildIdentifier(void);
 
 #ifdef __cplusplus
 } // extern "C"
