d93fec24602173c4c3f9231154cb4590b695d0dd
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2023-04-10 19:29:53 +0100

    Improved TestUtil headers

--- tests/tests_v2mpasm/src/ProgramVerification.cpp
+++ tests/tests_v2mpasm/src/ProgramVerification.cpp
@@ -4,31 +4,31 @@
 #include "ProgramVerification.h"
 #include "catch2/catch.hpp"
 
-static std::string WordAsHex(V2MP_Word in)
+static std::string WordAsHex(Asm::Word in)
 {
 	std::stringstream stream;
 	stream << "0x" << std::setw(4) << std::setfill('0') << std::hex << static_cast<uint32_t>(in);
 	return stream.str();
 }
 
-void CheckProgramMatches(V2MPAsm_Assembler* assembler, const std::vector<V2MP_Word>& expected)
+void CheckProgramMatches(V2MPAsm_Assembler* assembler, const std::vector<Asm::Word>& expected)
 {
 	REQUIRE(assembler);
 	REQUIRE(V2MPAsm_Assembler_GetExceptionCount(assembler) == 0);
 
-	CHECK(V2MPAsm_Assembler_InMemoryOutputBufferSizeInBytes(assembler) == expected.size() * sizeof(V2MP_Word));
+	CHECK(V2MPAsm_Assembler_InMemoryOutputBufferSizeInBytes(assembler) == expected.size() * sizeof(Asm::Word));
 
-	std::vector<V2MP_Word> actual(expected.size(), 0);
-	const size_t outputSize = V2MPAsm_Assembler_TakeInMemoryOutputBuffer(assembler, actual.data(), actual.size() * sizeof(V2MP_Word));
+	std::vector<Asm::Word> actual(expected.size(), 0);
+	const size_t outputSize = V2MPAsm_Assembler_TakeInMemoryOutputBuffer(assembler, actual.data(), actual.size() * sizeof(Asm::Word));
 
-	CHECK(outputSize == actual.size() * sizeof(V2MP_Word));
+	CHECK(outputSize == actual.size() * sizeof(Asm::Word));
 
 	// Just in case the above check fails - we still want to check as many words as we can.
 	const size_t count = std::min<size_t>(expected.size(), actual.size());
 
 	for ( size_t index = 0; index < count; ++index )
 	{
-		DYNAMIC_SECTION( "Verifying code word at address: " << (index * sizeof(V2MP_Word)) )
+		DYNAMIC_SECTION( "Verifying code word at address: " << (index * sizeof(Asm::Word)) )
 		{
 			// Convert the values to hex strings, to make it easier to interpret when a test fails.
 			CHECK(WordAsHex(expected[index]) == WordAsHex(actual[index]));
--- tests/tests_v2mpasm/src/ProgramVerification.h
+++ tests/tests_v2mpasm/src/ProgramVerification.h
@@ -4,4 +4,4 @@
 #include "LibV2MPAsm/Assembler.h"
 #include "TestUtil/Assembly.h"
 
-void CheckProgramMatches(V2MPAsm_Assembler* assembler, const std::vector<V2MP_Word>& expected);
+void CheckProgramMatches(V2MPAsm_Assembler* assembler, const std::vector<Asm::Word>& expected);
