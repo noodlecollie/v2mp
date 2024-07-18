ad9aaa3a5a9daf3f8b962369cd8d46cee844b1f3
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-07-18 16:11:06 +0100

    First skeleton window for instruction execution

--- src/main.zig
+++ src/main.zig
@@ -5,55 +5,62 @@ const capy = @import("capy");
 
 pub usingnamespace capy.cross_platform;
 
+fn executeInstruction(_: *anyopaque) !void {
+    std.debug.print("Execute button pressed\n", .{});
+}
+
 pub fn main() !void {
     try capy.init();
 
     var window = try capy.Window.init();
 
-    var monospace = capy.Atom(bool).of(false);
-    var text = capy.Atom([]const u8).of("");
-
-    const text_length = try capy.Atom(usize).derived(.{&text}, &struct {
-        fn callback(txt: []const u8) usize {
-            return txt.len;
-        }
-    }.callback);
-
-    var label_text = try capy.FormattedAtom(capy.internal.lasting_allocator, "Text length: {d}", .{text_length});
-    defer label_text.deinit();
-
-    try window.set(capy.column(.{ .spacing = 0 }, .{
-        capy.expanded(capy.textArea(.{})
-            .bind("monospace", &monospace)
-            .bind("text", &text)),
-        capy.label(.{ .text = "TODO: cursor info" })
-            .bind("text", label_text),
-        // TODO: move into menu
-        capy.checkBox(.{ .label = "Monospaced" })
-            .bind("checked", &monospace),
+    try window.set(capy.column(.{}, .{
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "R0" }),
+                capy.textField(.{ .readOnly = true, .name = "r0_field" }),
+            },
+        ),
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "R1" }),
+                capy.textField(.{ .readOnly = true, .name = "r1_field" }),
+            },
+        ),
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "LR" }),
+                capy.textField(.{ .readOnly = true, .name = "lr_field" }),
+            },
+        ),
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "PC" }),
+                capy.textField(.{ .readOnly = true, .name = "pc_field" }),
+            },
+        ),
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "SR" }),
+                capy.textField(.{ .readOnly = true, .name = "sr_field" }),
+            },
+        ),
+        capy.row(
+            .{ .expand = .Fill },
+            .{
+                capy.label(.{ .text = "Instruction" }),
+                capy.textField(.{ .name = "instruction_field" }),
+            },
+        ),
+        capy.button(.{ .label = "Execute", .onclick = executeInstruction }),
     }));
 
-    // TODO: hotkeys for actions (Ctrl+S, Ctrl+C) plus corresponding Cmd+C on macOS
-    window.setMenuBar(capy.menuBar(.{
-        capy.menu(.{ .label = "File" }, .{
-            capy.menuItem(.{ .label = "New File" }),
-            capy.menuItem(.{ .label = "Open File.." }),
-            capy.menuItem(.{ .label = "Save" }),
-            // TODO: capy.menuSeperator ?
-            capy.menuItem(.{ .label = "Quit" }),
-        }),
-        capy.menu(.{ .label = "Edit" }, .{
-            capy.menuItem(.{ .label = "Find" }),
-            capy.menuItem(.{ .label = "Copy" }),
-            capy.menuItem(.{ .label = "Paste" }),
-        }),
-        capy.menu(.{ .label = "View" }, .{
-            // TODO: togglemenuitem ?
-            capy.menuItem(.{ .label = "Monospace" }),
-        }),
-    }));
-
-    window.setTitle("Notepad");
+    window.setTitle("V2MP");
     window.setPreferredSize(800, 600);
     window.show();
 
