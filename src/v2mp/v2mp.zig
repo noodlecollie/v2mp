0c67cd904c3e113919ffeaaf86a5af6c0805f040
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-08-23 17:40:11 +0100

    Fixed compilation on Linux

--- src/v2mp/v2mp.zig
+++ src/v2mp/v2mp.zig
@@ -3,4 +3,4 @@ const std = @import("std");
 pub const defs = @import("defs.zig");
 pub const utils = @import("utils.zig");
 
-pub const Cpu = @import("cpu.zig");
+pub const Cpu = @import("Cpu.zig");
